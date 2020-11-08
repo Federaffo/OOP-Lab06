45429c9405f27ba641fce89d1093b63791c7e3b7
Federico Raffoni <36224988+Federaffo@users.noreply.github.com>
2020-11-08 02:20:48 +0100

    es collection 2 completato

--- src/it/unibo/oop/lab/collections2/SocialNetworkUserImpl.java
+++ src/it/unibo/oop/lab/collections2/SocialNetworkUserImpl.java
@@ -1,23 +1,14 @@
 package it.unibo.oop.lab.collections2;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-/**
- * 
- * Instructions
- * 
- * This will be an implementation of
- * {@link it.unibo.oop.lab.collections2.SocialNetworkUser}:
- * 
- * 1) complete the definition of the methods by following the suggestions
- * included in the comments below.
- * 
- * @param <U>
- *            Specific user type
- */
 public class SocialNetworkUserImpl<U extends User> extends UserImpl implements SocialNetworkUser<U> {
 
+	final Map<String, List<U>> contact;		//mappa che associa nome gruppo ai suoi partecipanti
     /*
      * 
      * [FIELDS]
@@ -40,6 +31,10 @@ public class SocialNetworkUserImpl<U extends User> extends UserImpl implements S
      * 
      * 2) Define a further constructor where age is defaulted to -1
      */
+	public SocialNetworkUserImpl(final String name, final String surname, final String user) {
+		super(name, surname, user);
+        contact = new HashMap<>();
+	}
 
     /**
      * Builds a new {@link SocialNetworkUserImpl}.
@@ -56,27 +51,57 @@ public class SocialNetworkUserImpl<U extends User> extends UserImpl implements S
      */
     public SocialNetworkUserImpl(final String name, final String surname, final String user, final int userAge) {
         super(name, surname, user, userAge);
+        contact = new HashMap<>();
     }
-
-    /*
-     * [METHODS]
-     * 
-     * Implements the methods below
-     */
 
     @Override
     public boolean addFollowedUser(final String circle, final U user) {
-        return false;
+    	
+    	boolean exist = true;
+    	
+    	for(List<U> l : contact.values()) {
+    		if(l.contains(user)) {
+    			exist = false;
+    		}
+    	}
+    	
+    	if(contact.containsKey(circle)) {
+    		contact.get(circle).add(user);
+    	}else {
+    		final List<U> temporalyGroup = new ArrayList();
+    		temporalyGroup.add(user);
+    		contact.put(circle, temporalyGroup);
+    	}
+    	
+        return exist;
     }
 
     @Override
     public Collection<U> getFollowedUsersInGroup(final String groupName) {
-        return null;
+    	
+    	if(contact.containsKey(groupName)) {
+    		List<U> temporalyList = contact.get(groupName);
+    		return new ArrayList<U>(contact.get(groupName));
+    	}else {
+    		return new ArrayList<>();
+    	}
+        
     }
 
     @Override
     public List<U> getFollowedUsers() {
-        return null;
+    	
+    	List<U> temporalyList = new ArrayList<>();
+    	
+    	for(List<U> l : contact.values()) {
+    		for(U x : l) {
+    			if(!temporalyList.contains(x)) {
+    				temporalyList.add(x);
+    			}
+    		}
+    	}
+    	
+        return temporalyList;
     }
 
 }
